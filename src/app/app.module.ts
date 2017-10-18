bf9aa9dda6545e85cba50ec1ce9e5474e020e93d
Gautam <gautamvelpula@gmail.com>
2017-10-17 21:56:08 -0400

    Added modules for game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { CreateGameComponent } from './game/create/creategame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    CreateGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'game', component: GameComponent },
+      { path: 'game/create', component: CreateGameComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
